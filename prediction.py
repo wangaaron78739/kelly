16fe327c033a2c9949f9736e8977d26940e2db15
taksum <chantaksum1999@gmail.com>
2021-05-11 19:35:35 +0800

    logging update + clean up

--- prediction.py
+++ prediction.py
@@ -21,7 +21,7 @@ class Prediction:
 
     # execution params
     execution_block = 4         # transaction is fired when lock block <= n block away
-    gas_price = 6
+    gas_price = 5
     gas = 200000
 
     # logger
@@ -66,8 +66,6 @@ class Prediction:
         """Build and send a transaction."""
         if tx_params is None:
             tx_params = self._get_tx_params()
-        print(function)
-        print(tx_params)
         tx = function.buildTransaction(tx_params)
         signed_txn = self.w3.eth.account.sign_transaction(tx, private_key=self.private_key)
         try:
@@ -79,53 +77,19 @@ class Prediction:
     def place_bet(self, bet_size, direction):
         if bet_size is None or direction is None:
             return None
-        if bet_size < 0.001:
-            return None
-        data = {'BULL': '0x821daba1', 'BEAR': '0x0088160f'}
-        tx = {
-            'from': self.address,
-            'to': self.contract.address,
-            'value': self.w3.toWei(bet_size, 'ether'),
-            'gas': self.gas,
-            'gasPrice': self.w3.toWei(self.gas_price, 'gwei'),
-            'data': data[direction],
-            'nonce': self.w3.eth.get_transaction_count(self.address),
-        }
-        signed_tx = self.w3.eth.account.sign_transaction(tx, private_key)
-        return self.w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+        bet_functions = {'BULL': self.contract.functions.BetBull, 'BEAR': self.contract.functions.BetBear}
+        return self._build_and_send_tx(bet_functions[direction](bet_size))
 
-    def place_bet_v2(self, bet_size, direction):
-        if bet_size is None or direction is None:
+    def claim_rewards(self, epoch, gas=120000, gas_price=5):
+        if epoch < 0 or not self.contract.functions.claimable(epoch, self.address).call():
             return None
-        if bet_size < 0:
-            return None
-        function_dict = {'BULL': self.contract.functions.BetBull, 'BEAR': self.contract.functions.BetBear}
-        return self._build_and_send_tx(function_dict[direction](bet_size))
         
-    def claim_rewards(self, epoch, gas=122200, gas_price=5):
-        if epoch == 0 or not self.contract.functions.claimable(epoch, self.address).call():
-            return None
-        tx = {
-            'nonce': self.w3.eth.get_transaction_count(self.address),
-            'from': self.address,
-            'to': self.contract.address,
-            'gas': gas,
-            'gasPrice': self.w3.toWei(gas_price, 'gwei'),
-            'data': '0x379607f5' + self.w3.toHex(epoch)[2:].rjust(64,'0')
-        }
-        signed_tx = self.w3.eth.account.sign_transaction(tx, private_key)
-        tx_hash = self.w3.eth.send_raw_transaction(signed_tx.rawTransaction)
-        return self.w3.eth.wait_for_transaction_receipt(tx_hash)
-
-    def claim_rewards_v2(self, epoch, gas=122200, gas_price=5):
-        if epoch == 0 or not self.contract.functions.claimable(epoch, self.address).call():
-            return None
-        tx_params = self._get_tx_params()
-        tx_params['gas'] = gas
-        tx_params['gasPrice'] = self.w3.toWei(gas_price, 'gwei')
         return self._build_and_send_tx(
             self.contract.functions.claim(epoch),
-            tx_params
+            self._get_tx_params() | {
+                'gas': gas,
+                'gasPrice': self.w3.toWei(gas_price, 'gwei')
+            }
         )
 
     def compute_kelly(self, bull_odd, bear_odd):
@@ -137,12 +101,11 @@ class Prediction:
         
         curr_epoch = self.contract.functions.currentEpoch().call()
         prev_epoch = curr_epoch-1
-        bet_on = False
-        
+
         while True:
 
             curr_epoch = self.contract.functions.currentEpoch().call()
-            bet_on = bet_on if curr_epoch == prev_epoch else False
+            bet_on = False if curr_epoch != prev_epoch else bet_on
             balance = self.balance_override if self.balance_override > 0 else float(
                 self.w3.fromWei(self.w3.eth.get_balance(self.address), 'ether')) - self.gas_fee_reserve
 
@@ -155,7 +118,8 @@ class Prediction:
             total_amount = rounds[6]
             
             if blocks_away > 50:
-                if receipt := self.claim_rewards(prev_epoch-1):
+                tx_hash = self.claim_rewards(prev_epoch-1)
+                if receipt := self.w3.eth.wait_for_transaction_receipt(tx_hash):
                     self.logger.info(f"Claim status: {receipt['status']}")
 
             if bull_amount > 0 and bear_amount > 0:
@@ -165,12 +129,7 @@ class Prediction:
                 bull_kelly, bear_kelly = self.compute_kelly(bull_odd=bull_odd,bear_odd=bear_odd)
                 prize_pool = float(self.w3.fromWei(total_amount, 'ether'))
                 
-                print(f'Round: {curr_epoch:10} | Blocks Away: {blocks_away}')
-                print(f'Bull Odds: {bull_odd:6.3f} | Kelly: {bull_kelly:6.0%}')
-                print(f'Bear Odds: {bear_odd:6.3f} | Kelly: {bear_kelly:6.0%}')
-                print(f'Prize Pool: {prize_pool:.3f}\n')
-                print(f'Balance: {balance:.3f}\n')
-
+                self.logger.info(f'Round: {curr_epoch} | Blocks Away: {blocks_away} | Bull Odds: {bull_odd:.3f} | Bull Kelly: {bull_kelly:.0%} | Bear Odds: {bear_odd:.3f} | Bear Kelly: {bear_kelly:.0%} | Prize Pool: {prize_pool:.3f} | Balance: {balance:.3f}')
                 direction = 'BULL' if bull_kelly > bear_kelly else 'BEAR'
                 bet_size = balance*min(max(bull_kelly, bear_kelly), self.kelly_cap)
                 
@@ -193,5 +152,9 @@ class Prediction:
 
 
 if __name__ == '__main__':
-    prediction = Prediction(address, private_key)
-    prediction.start()
+    
+    logging.basicConfig(level = logging.INFO, format=('[%(levelname)s] %(message)s'))
+    
+    Prediction(address, private_key).start()
+    
+    logging.shutdown()
